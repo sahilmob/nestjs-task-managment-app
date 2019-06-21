6cf3f81d9735ffe82dd481a41c5332abaea07d7d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-21 19:53:41 +0300

    handle not found task on delete

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -58,6 +58,7 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 }

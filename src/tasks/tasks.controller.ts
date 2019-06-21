8d52c757117bb5bb7768b5d77418654bafae7f2a
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-21 17:58:42 +0300

    handle delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from '../../dist/tasks/task.model';
@@ -21,5 +21,10 @@ export class TasksController {
   @Post()
   createTasK(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
   }
 }

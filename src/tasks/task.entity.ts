4f99d1ec948e6960c7dfcd64acbe0e409f6c5ad3
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 15:54:29 +0300

    fix task status import

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,6 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-import { TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
 
 @Entity()
 export class Task extends BaseEntity {

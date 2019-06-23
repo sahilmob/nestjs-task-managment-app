25d23cae85ada58160d95d06483e554c15d00d63
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 16:04:22 +0300

    fix task status enum import

--- src/tasks/pipes/task-status-validation.pipes.ts
+++ src/tasks/pipes/task-status-validation.pipes.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, PipeTransform } from '@nestjs/common';
 
-import { TaskStatus } from '../task.model';
+import { TaskStatus } from '../task-status.enum';
 
 export class TaskStatusValidationPipe implements PipeTransform {
   readonly allowedStatuses = [

f61846a5f27245d07155b4317ce6b5e1c2ba3a71
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-06-23 15:55:16 +0300

    fix task status import

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,6 @@
 import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 
-import { TaskStatus } from '../task.model';
+import { TaskStatus } from '../task-status.enum';
 
 export class GetTasksFilterDto {
   @IsOptional()

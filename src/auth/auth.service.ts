87682e3d0e37d68a1e8d686ec41b043bcab6d40e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-06 12:49:28 +0300

    inject user repository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+}

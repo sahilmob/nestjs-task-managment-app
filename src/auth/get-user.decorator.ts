4f38ae895635676025c97ea09cc62af978300c79
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-07 16:47:25 +0300

    init

---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,8 @@
+import { User } from './user.entity';
+import { createParamDecorator } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data, req): User => {
+    return req.user;
+  },
+);

04971554ee10026057f467911191736ddead2008
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-07 16:40:40 +0300

    init

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { JwtPayload } from '../../dist/auth/jwt-payload.interface';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'topSecret',
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
